6bf194c9d6917c188b88e62189d35aaef4601ca4
STARTRACEX <startraceebr@foxmail.com>
2022-06-23 14:34:32 +0800

    pul

--- js/index.js
+++ js/index.js
@@ -98,17 +98,23 @@ function Footer() {
     document.write("                    <\/ul>");
     document.write("                <\/li>");
     document.write("                <li class=\"footertab\">");
-    document.write("                    <span>其他<\/span>");
+    document.write("                    <span i18n=\"Other\">其他<\/span>");
     document.write("                    <ul>");
-    document.write("                        <li><a href=\".\/doc.html\">转到文献库<\/a><\/li>");
+    document.write("                        <li><a href=\".\/doc.html\" i18n=\"Literature\">转到文献库<\/a><\/li>");
     document.write("                    <\/ul>");
-    document.write("");
+    document.write("                <\/li>");
+    document.write("                <li class=\"footertab\">");
+    document.write("                    <span i18n=\"Link\">链接<\/span>");
+    document.write("                    <ul>");
+    document.write("                        <li><a href=\".\/index.html\">STARTRACE<\/a><\/li>");
+    document.write("                        <li><a href=\"\" >STARTRACE IO<\/a><\/li>");
+    document.write("                    <\/ul>");
     document.write("                <\/li>");
     document.write("            <\/ul>");
     document.write("            <div class=\"footer-cannal\" style=\"display: block;\">");
     document.write("                <a class=\"footerqr\" href=\"https:\/\/github.com\/STARTRACEX\" target=\"_blank\">");
-    document.write("                    <svg t=\"1655171075206\" class=\"icon\" viewBox=\"0 0 1024 1024\" version=\"1.1\"");
-    document.write("                        xmlns=\"http:\/\/www.w3.org\/2000\/svg\" p-id=\"2859\" width=\"36\" height=\"36\">");
+    document.write("                    <svg t=\"1655171075206\" class=\"icon\" viewBox=\"0 0 1024 1024\" version=\"1.1\" p-id=\"2859\" width=\"36\"");
+    document.write("                        height=\"36\">");
     document.write("                        <path");
     document.write("                            d=\"M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9 23.5 23.2 38.1 55.4 38.1 91v112.5c0.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z\"");
     document.write("                            p-id=\"2860\" fill=\"#f4f4f4\"><\/path>");
@@ -116,7 +122,7 @@ function Footer() {
     document.write("                <\/a>");
     document.write("                <a class=\"footerqr\" href=\"#footer\">");
     document.write("                    <svg style=\"transform: translateY(1.2px);\" t=\"1655135087965\" class=\"icon\" viewBox=\"0 0 1024 1024\"");
-    document.write("                        version=\"1.1\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\" p-id=\"8317\" width=\"36\" height=\"36\">");
+    document.write("                        version=\"1.1\" p-id=\"8317\" width=\"36\" height=\"36\">");
     document.write("                        <path");
     document.write("                            d=\"M684.2624 336.639078c10.256998 0 20.405555 0.753152 30.472294 1.873715C687.354982 211.026227 551.049933 116.304896 395.467162 116.304896c-173.940736 0-316.424704 118.557184-316.424704 269.103923 0 86.8992 47.403622 158.259405 126.61801 213.608038l-31.648051 95.184896 110.615859-55.468339c39.581082 7.834419 71.337677 15.888896 110.837965 15.888896 9.929523 0 19.775181-0.490189 29.539021-1.257677-6.182093-21.154816-9.763738-43.312435-9.763738-66.302054C415.242342 448.825139 533.955994 336.639078 684.2624 336.639078zM514.125517 250.849178c23.825613 0 39.60873 15.673958 39.60873 39.484314 0 23.715123-15.783117 39.607091-39.60873 39.607091-23.716147 0-47.513088-15.891968-47.513088-39.607091C466.612429 266.522112 490.40937 250.849178 514.125517 250.849178zM292.671693 329.939456c-23.713075 0-47.648154-15.891968-47.648154-39.607091 0-23.810253 23.934054-39.484314 47.648154-39.484314 23.715123 0 39.499264 15.673958 39.499264 39.484314C332.170957 314.04759 316.386816 329.939456 292.671693 329.939456zM973.059686 583.260979c0-126.504141-126.593434-229.620736-268.773478-229.620736-150.553088 0-269.128499 103.116493-269.128499 229.620736 0 126.72215 118.576538 229.620736 269.128499 229.620736 31.510938 0 63.294157-7.945933 94.942208-15.877632l86.79127 47.525376-23.796941-79.073894C925.73696 717.806285 973.059686 654.621286 973.059686 583.260979zM617.027379 543.668224c-15.755469 0-31.648051-15.673958-31.648051-31.661056 0-15.766118 15.892582-31.643648 31.648051-31.643648 23.934054 0 39.60873 15.877632 39.60873 31.643648C656.636109 527.994266 640.961434 543.668224 617.027379 543.668224zM791.077581 543.668224c-15.647027 0-31.430144-15.673958-31.430144-31.661056 0-15.766118 15.783117-31.643648 31.430144-31.643648 23.715123 0 39.605658 15.877632 39.605658 31.643648C830.684262 527.994266 814.793626 543.668224 791.077581 543.668224z\"");
     document.write("                            p-id=\"8318\" fill=\"#f4f4f4\"><\/path>");
@@ -145,27 +151,15 @@ function Footer() {
     document.write("                <\/a>");
     document.write("            <\/div>");
     document.write("        <\/div>");
-    document.write("        <div id=\"footer-beian\">");
+    document.write("        <div id=\"footer-cookie\">");
     document.write("            <a href=\"http:\/\/www.beian.gov.cn\/\" target=\"_blank\" i18n=\"llpb\"> &nbsp; <\/a>");
     document.write("            <a href=\".\/cookie.html\" target=\"_blank\">Cookie<\/a>");
     document.write("        <\/div>");
     document.write("        <div id=\"footer-copyright\">");
     document.write("            <a href=\"\">&copy;STARTRACE<\/a>");
-    document.write("            <a href=\"https:\/\/github.com\/STARTRACEX\" i18n=\"pagedesine\">页面设计：计科2101王世超<\/a>");
+    document.write("            <a href=\"https:\/\/github.com\/STARTRACEX\" i18n=\"pagedesine\">页面设计：王世超<\/a>");
     document.write("        <\/div>");
     document.write("    <\/footer>");
-
-    document.write("<div id=\"totop\">");
-    document.write("        <a href=\"\">");
-    document.write("            <svg t=\"1650535477964\" class=\"icon\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"#\" p-id=\"5015\" width=\"36\"");
-    document.write("                height=\"36\">");
-    document.write("                <path");
-    document.write("                    d=\"M484.538555 297.401434 78.508052 703.407378c-11.784394 11.813047-11.784394 30.912034 0 42.724057 11.81714 11.81407 30.94171 11.81407 42.757826 0l386.211109-386.181433 379.830795 379.806235c11.453866 11.482519 30.039153 11.482519 41.491996 0 11.511171-11.453866 11.511171-30.039153 0-41.516556L534.372543 303.776631c-1.543146-1.539053-3.417843-2.296299-5.200442-3.412726-0.691755-0.935302-1.055029-2.085498-1.933025-2.962471-11.784394-11.784394-30.912034-11.784394-42.695405 0L484.538555 297.401434zM484.538555 297.401434\"");
-    document.write("                    p-id=\"5016\" fill=\"#f7f7f7\"><\/path>");
-    document.write("            <\/svg>");
-    document.write("        <\/a>");
-    document.write("    <\/div>");
-
 }
 //加载动画，在任何地方调用，需要jqury
 function LoadingKf() {
